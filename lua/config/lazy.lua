48a929c931dee9cd8a282c35c8574f3f1ab46734
ovftank <ovftank@gmail.com>
2025-06-08 19:57:03 +0700

    fix: improve error messages for lazy.nvim cloning

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,12 +1,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.fn.isdirectory(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "cant clone lazy.nvim:\n", "ErrorMsg" },
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
-      { "\npress any key to exit..." },
+      { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
